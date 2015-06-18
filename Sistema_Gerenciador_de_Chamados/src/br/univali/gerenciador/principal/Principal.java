eeb4f8cec0cf7423c3a0e5ef7a705dfc8ba0eae4
João <Joo@192.168.25.4>
2015-06-17 21:55:35 -0300

    Pulei uma linha.

--- Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/principal/Principal.java
+++ Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/principal/Principal.java
@@ -2,6 +2,7 @@
  *  UNIVALI - Universidade do Vale do Itajaí
  *  Curso de bacharelado e Ciência da Computação
  *  Disciplina de Banco de Dados I
+ *  
  *  Acadêmicos: -   Ailton Cardoso Júnior
                 -   João Felipe Gonçalves
  */
