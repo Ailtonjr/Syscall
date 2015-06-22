bbe64a63d48481d04928a7f34abdd6c6f5b7deb4
João <Joo@192.168.25.168>
2015-06-22 15:58:54 -0300

    Driver JDBC PostgreSQL adicionado. Conexão com banco realizada.

--- Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/modelo/Conexao.java
+++ Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/modelo/Conexao.java
@@ -4,13 +4,10 @@ package br.univali.gerenciador.modelo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 
 public class Conexao {
 
-    private String url = "jdbc:postgresql://localhost:5432/db_chamados";
+    private String url = "jdbc:postgersql://localhost:5432/db_chamados";
     private String usuario = "us_admin";
     private String senha = "1234";
     private Connection conexao;
@@ -20,7 +17,6 @@ public class Conexao {
             conexao = DriverManager.getConnection(url, usuario, senha);
             System.out.println("Conexão estabelecida");
         } catch (SQLException ex) {
-            //Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Erro ao se conectar");
         }
     }
