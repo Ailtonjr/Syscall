04c116336955603619d74c499abc78cf90dd6996
João <Joo@192.168.25.168>
2015-06-22 15:43:19 -0300

    Remoção de mains

---
+++ Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/modelo/Conexao.java
@@ -0,0 +1,30 @@
+
+package br.univali.gerenciador.modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Conexao {
+
+    private String url = "jdbc:postgresql:db_chamados";
+    private String usuario = "us_admin";
+    private String senha = "1234";
+    private Connection conexao;
+    
+    public Conexao() {
+        try {
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão estabelecida.");
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+    
+}
--- Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/visao/NovoCliente.java
+++ Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/visao/NovoCliente.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.univali.gerenciador.visao;
 
-/**
- *
- * @author Ailton Cardoso Jr
- */
+
 public class NovoCliente extends javax.swing.JDialog {
 
     /**
@@ -17,6 +10,19 @@ public class NovoCliente extends javax.swing.JDialog {
     public NovoCliente(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                NovoCliente dialog = new NovoCliente(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
     }
 
     /**
@@ -122,18 +128,7 @@ public class NovoCliente extends javax.swing.JDialog {
      */
     public static void main(String args[]) {
         /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                NovoCliente dialog = new NovoCliente(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
-        });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
