d22430011e77e689224df00e1c43d9d0ccefe359
Ailton Cardoso Jr <Ailton Cardoso Jr@Ailton_Dell>
2015-06-25 15:55:10 -0300

    Merge origin/João
    Conflicts:
    	Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/visao/Login.form

--- Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/visao/Login.java
+++ Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/visao/Login.java
@@ -112,27 +112,12 @@ public class Login extends javax.swing.JFrame {
         label_Titulo.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
         label_Titulo.setText("Sistema de Chamado");
 
-        tabela.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(tabela);
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addComponent(label_logo)
@@ -233,7 +218,6 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JPasswordField fild_Password;
     private javax.swing.JTextField fild_User;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel label_Login;
     private javax.swing.JLabel label_Password;
     private javax.swing.JLabel label_Titulo;
