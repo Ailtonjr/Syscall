0d217cab57ec5a55cbb063002e0189de1f19bdf8
João <Joo@JFRode>
2015-06-26 18:16:00 -0300

    Removido algumas linhas extras.

--- Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/modelo/Conexao.java
+++ Sistema_Gerenciador_de_Chamados/src/br/univali/gerenciador/modelo/Conexao.java
@@ -6,7 +6,6 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 public class Conexao {
@@ -346,7 +345,5 @@ public class Conexao {
             ex.printStackTrace();
         }
         return rs;
-    }
-    
-    
+    }  
 }
